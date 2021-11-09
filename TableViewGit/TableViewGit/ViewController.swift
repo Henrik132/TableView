7f46715afb8a7963f12915fe5a7e669d8f06dbef
Henrik Lüdemann / BBS2H20A <Henrik.Luedemann@edu.bib.de>
2021-11-09 09:05:12 +0100

    TableView angelegt, DataSource & Delegate , Navigation Coontroller Edit + Title

--- TableViewGit/TableViewGit/ViewController.swift
+++ TableViewGit/TableViewGit/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+
+        var games = ["Far Cry", "hduashu", "jsioffjsd", "fids", "djaij"]
+    
+        
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myTableView.dataSource = self
+        myTableView.delegate = self
+
+
+        navigationItem.rightBarButtonItem = editButtonItem
+        
+        title = "Games"
+
     }
 
-
+    
+    
 }
 
+
+    extension ViewController: UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return games.count
+        }
+    
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let myCell = myTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+            
+            myCell.textLabel?.text = games[indexPath.row]
+            
+            return myCell
+        }
+        
+        
+    }
+
+    extension ViewController: UITableViewDelegate {
+    
+    }
